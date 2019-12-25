e48988f0fe6e42b0d7617343a46f845a5dbe341b
Roger Esteve <Coconaut7@gmail.com>
2019-12-25 19:36:48 +0100

    Finished

---
+++ update.py
@@ -0,0 +1,25 @@
+import winreg
+import struct
+
+val = "43 00 08 10 19 00 00 00" # Just to for reference
+
+path = "System\CurrentControlSet\Control\MediaProperties\PrivateProperties\Joystick\OEM\VID_046D&PID_C24F"
+
+names = ["OEMData", "OEMName"]
+types = [winreg.REG_BINARY, winreg.REG_SZ]
+values = [struct.pack('<Q', int("0x1910080043", 16)), "Logitech G29 Driving Force Racing Wheel USB"]
+
+def main():
+    try:
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path, 0, winreg.KEY_WRITE)
+        for i in range(2):
+            winreg.SetValueEx(key, names[i], 0, types[i], values[i])
+            print(names[i] + " -> " + str(values[i]))
+
+        winreg.CloseKey(key)
+        print("Key updated!")
+    except:
+        print("Could not open the key (HKEY_CURRENT_USER\\" + path + ")")
+    
+    
+main()
